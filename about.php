72d5b79bd4284290beb0527fc113a26c3bf3f488
siamcodes <somkiat@gmail.com>
2022-06-29 19:15:02 +0700

    first commit

---
+++ about.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/bootstrap.css" />
+    <script src="./js/bootstrap.js"></script>
+
+</head>
+
+<body>
+    <div class="container-fluid">
+        <?php
+        include "navbar.php";
+        echo "<h1> เกี่ยวกับเรา </h1>";
+        ?>
+    </div>
+</body>
+
+</html>
---
+++ navbar.php
@@ -0,0 +1,4 @@
+
+<a href="index.php"> หน้าหลัก </a>
+<a href="contact.php"> ติดต่อเรา </a>
+<a href="about.php"> เกี่ยวกับเรา </a>
