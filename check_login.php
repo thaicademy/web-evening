3de98ba9897caac3ee44d46c3bc3f386e7f72c78
siamcodes <somkiat@gmail.com>
2022-07-22 19:30:09 +0700

    edit

--- check_login.php
+++ check_login.php
@@ -1,20 +1,27 @@
 <?php
   session_start();
   include "connect.php";
- // $sql = "SELECT * FROM tbl_users WHERE 
- //        username='$_POST[username]' and password='$_POST[password]' ";
-  $sql = "SELECT * FROM tbl_users";
-  $result = mysqli_query($conn, $sql);
-  $row = mysqli_fetch_assoc($result);
 
-  if($row['username'] == $_POST['username'] && $row[password] == $_POST['password']){
-      $_SESSION['username'] = $row['username'];
-      header("location: ./dashboard.php");
+  if(!empty($_POST['username']) && !empty($_POST['password']) ){
+      $sql = "SELECT * FROM tbl_users WHERE 
+            username='$_POST[username]'  && password ='$_POST[password]' ";
+      $result = mysqli_query($conn, $sql);
+      $row = mysqli_fetch_array($result);
+
+      if($row['username'] == $_POST['username'] && $row[password] == $_POST['password']){
+          $_SESSION['username'] = $row['username'];
+          header("location: ./dashboard.php");
+      }else{
+        echo "<script>
+                alert('Username หรือ Password ไม่ถูกต้อง');
+                window.location.href='./login.php';
+              </script>";
+      }
   }else{
-     echo "<script>
-             alert('Username หรือ Password ไม่ถูกต้อง');
-             window.location.href='./login.php';
-           </script>";
+    echo "<script>
+        alert('Username หรือ Password ไม่ถูกต้อง');
+        window.location.href='./login.php';
+    </script>";
   }
 
 ?>
--- connect.php
+++ connect.php
@@ -1,11 +1,11 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "dindang1";
+$user = "root";
+$pass = "dindang1";
 $dbname = "evening";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $user, $pass, $dbname);
 
 // Check connection
 if (!$conn) {
