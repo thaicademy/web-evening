9d831d91ff9cd058362df5bddd2b9ccf51fe4632
siamcodes <somkiat@gmail.com>
2022-08-09 19:55:12 +0700

    init

---
+++ prd_insert.php
@@ -0,0 +1,14 @@
+<?php
+include "connect.php";
+
+$sql = "INSERT INTO tbl_products(name, description, price, stock, category, img) VALUES ('$_POST[name]', '$_POST[description]', '$_POST[price]', '$_POST[stock]', '$_POST[category]', '$_POST[img]' )";
+
+if (mysqli_query($conn, $sql)) {
+  echo "บันทึกเรียบร้อย";
+  header("location: ./prd_select.php");
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
