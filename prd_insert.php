c3f6acf84835a5b6a0372430e9b1d954614acb92
siamcodes <somkiat@gmail.com>
2022-08-11 18:25:01 +0700

    init

--- prd_insert.php
+++ prd_insert.php
@@ -1,9 +1,14 @@
 <?php
 include "connect.php";
 
-$sql = "INSERT INTO tbl_products(name, description, price, stock, category, img) VALUES ('$_POST[name]', '$_POST[description]', '$_POST[price]', '$_POST[stock]', '$_POST[category]', '$_POST[img]' )";
+$filename = $_FILES["img"]["name"];
+$tempname = $_FILES["img"]["tmp_name"];
+$folder = "./images/" . $filename;
+
+$sql = "INSERT INTO tbl_products(name, description, price, stock, category, img) VALUES ('$_POST[name]', '$_POST[description]', '$_POST[price]', '$_POST[stock]', '$_POST[category]', '$filename' )";
 
 if (mysqli_query($conn, $sql)) {
+  move_uploaded_file($tempname, $folder);  //อับโหลดไฟล์   
   echo "บันทึกเรียบร้อย";
   header("location: ./product.php");
 } else {
