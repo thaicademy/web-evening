d1f514328b6b65880b9da9d9112ba1d0427143b3
siamcodes <somkiat@gmail.com>
2022-08-11 19:21:45 +0700

    init

--- prd_delete.php
+++ prd_delete.php
@@ -1,11 +1,18 @@
 <?php
 include "connect.php";
-$id = $_GET[id];
+$id = $_GET['id'];
+$img = $_GET['img'];
+ 
+if (file_exists('./images/'.$img)) {
+    unlink('./images/'.$img);
+    echo 'File '.$img.' has been deleted';
+} else {
+    echo 'Could not delete '.$img.', file does not exist';
+}
 
 $sql = "DELETE FROM tbl_products WHERE id='$id'";
 
 if (mysqli_query($conn, $sql)) {
-    
     echo "<script>
             alert('แก้ไขเรียบร้อย');
             window.location.href='./product.php';
@@ -14,6 +21,6 @@ if (mysqli_query($conn, $sql)) {
   echo "ผิดพลาด : " . mysqli_error($conn);
 }
 
-mysqli_close($conn);
+mysqli_close($conn); 
 
 ?>
